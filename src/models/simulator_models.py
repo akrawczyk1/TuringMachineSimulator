4133246fe4e37052e82c9f6b669e0a8de98395c9
andrewkrawczyk <akrawczyk@ufl.edu>
2026-03-09 15:22:54 -0400

    Creates basic simulator logic (without error checking)

--- src/models/simulator_models.py
+++ src/models/simulator_models.py
@@ -14,15 +14,28 @@ class TransitionAction(BaseModel):
     next_state: str
 
 class TuringMachine(BaseModel):
+    """
+    The initial configuration of the Turing Machine.
+
+    transitions should be a list with [state,read_char] as the lookup key
+
+    alphabet should not include the blank symbol.
+
+    blank_symbol should only be included if the user wants the UI to default to a symbol other than "_".
+    """
     states: List[str]  # list of state names
-    initial_state: str
-    accept_states: List[str]
-    reject_states: List[str]
-    transitions: Dict[str, TransitionAction]
-    alphabet: List[str]
-    blank_symbol: str
+    initial_state: str  # the name of the initial state
+    accept_states: List[str]  # the names of the accept states
+    reject_states: List[str]  # the names of the reject states
+    transitions: Dict[str, TransitionAction]  # [key: "state,read_char", value: TransitionAction
+    alphabet: List[str]  # all symbols the machine should read other than the blank symbol
+    blank_symbol: str = "_"
 
 class Tape(BaseModel):
+    """
+    fill_symbol is to be used if you want the edges of your input to be filled with symbols other than blanks.
+    For example, if you wanted an infinite amount of '0' on the edges of your tape, fill_symbol should be set to '0'.
+    """
     tape_contents: List[str]
     start_head_position: int
     fill_symbol: str
--- src/simulator.py
+++ src/simulator.py
@@ -1,18 +1,71 @@
-from src.models.simulator_models import *
+from src.models.simulator_models import TransitionAction, TuringMachine, Tape
 
 class Simulator:
     def __init__(self, machine: TuringMachine, tape: Tape):
+        self.NUM_MAX_STEPS = 1000
+
         self.states = machine.states
-        self.initial_state = machine.initial_state
+        self.current_state = machine.initial_state
         self.accept_states = machine.accept_states
         self.reject_states = machine.reject_states
         self.transitions = machine.transitions
-        self.alphabet = machine.alphabet
+        self.machine_alphabet = machine.alphabet
         self.blank_symbol = machine.blank_symbol
 
         self.tape_contents = tape.tape_contents
         self.head_position = tape.start_head_position
         self.fill_symbol = tape.fill_symbol
+
+        self.in_accept_state = False
+        self.in_reject_state = False
+
+        if self.fill_symbol is None:
+            self.fill_symbol = self.blank_symbol
+
+        if not self.check_tape_alphabet():
+            raise RuntimeError(
+                "The tape alphabet is not a subset of the machine alphabet."
+            )
+
+        self.set_accept_reject_state()
+
+    def check_tape_alphabet(self) -> bool:
+        tape_alphabet = set(self.tape_contents)
+        tape_alphabet.add(self.fill_symbol)
+
+        return tape_alphabet <= set(self.machine_alphabet)
+
+    def craft_lookup_key(self, current_symbol: str) -> str:
+        lookup_key = self.current_state + "," + current_symbol
+        return lookup_key
+
+    def set_accept_reject_state(self):
+        self.in_accept_state = self.current_state in self.accept_states
+        self.in_reject_state = self.current_state in self.reject_states
+
+    def step(self):
+        # Read symbol at current location
+        current_symbol = self.tape_contents[self.head_position]
+
+        # Look up the appropriate transition function
+        lookup_key = self.craft_lookup_key(current_symbol)
+        transition_action = self.transitions.get(lookup_key)
+
+        # Write to tape according to current transition function
+        self.tape_contents[self.head_position] = transition_action.write_symbol
+
+        # Move head according to transition function:
+        if transition_action.move_direction.upper() == 'R':
+            self.head_position += 1
+        elif transition_action.move_direction.upper() == 'L':
+            self.head_position -= 1
+        else:
+            pass
+
+        # Update the current state
+        self.current_state = transition_action.next_state
+        self.set_accept_reject_state()
+
 
 
 
